0b7b36dc97c2db69943be3cb83f81da83e0f398e
Shitab Mir <shitab.islam@alibaba-inc.com>
2024-02-29 12:37:05 +0600

    WIP Compose Activity

--- app/src/main/java/com/shitabmir/everythingio/presentation/buttonpage/ButtonsComposeActivity.kt
+++ app/src/main/java/com/shitabmir/everythingio/presentation/buttonpage/ButtonsComposeActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ButtonElevation
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shitabmir.everythingio.presentation.buttonpage.ui.theme.EverythingIOTheme
 
 class ButtonsComposeActivity : ComponentActivity() {
@@ -42,10 +53,29 @@ class ButtonsComposeActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val buttonColor1 by remember { mutableStateOf(Color.Red) }
+    Column(
+
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(
+            content = {
+                Text(text = "d")
+            },
+            onClick = { /*TODO*/ },
+            colors = ButtonColors(
+                containerColor = buttonColor1,
+                disabledContainerColor = Color.Gray,
+                contentColor = Color.White,
+                disabledContentColor = Color.White,
+            ),
+            enabled = true,
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
